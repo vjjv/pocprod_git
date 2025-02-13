181002906a2b01640c9b2fdfb96875fc45883370
Bastien Sarolea <sarobastien@gmail.com>
2025-02-13 16:19:05 +0100

    01

--- docs/packed.js
+++ docs/packed.js
@@ -30357,13 +30357,12 @@ let mediaStream;
 
 async function init() {
     const cameraKit = await bootstrapCameraKit({
-        // apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4MDU3NzAyLCJzdWIiOiI0MDUyY2RlNC02YzMzLTRkM2UtYTJjNC0yNzllYzc1M2VmOWR-U1RBR0lOR341MTY4YzVmNC1kYWVkLTQ1N2ItOGJmYy01Y2JhODkwOWU4OTgifQ.b0Z-TegYa2Sg-lZy_8XoPw7f_iz7eEC5BtzYooyL5K4',
-        apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM4MjM2Njg5LCJzdWIiOiJmYWMzYWZjOS0zOTEyLTRlNTUtYTdiZS03MjJlOGRmYWY4ZjV-UFJPRFVDVElPTn5lOGQ0OTM1NS00YmNlLTRiYWEtODkzNC1lMWNlNmU0ZDM5M2IifQ.6sZB_6aFPL8OW-UO3Y37P7Rev7mzjS9IhNRFk7NelBI',
+        apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM5NDU4Nzc3LCJzdWIiOiJjMGE2ODQ5OC1hZTU4LTQzMDctODcwMy00MTZhM2YzYTNmNDZ-U1RBR0lOR344YTRmOTliMC1lODk4LTQ1MzYtOTllMS0wNDIyMmFhMjJkYTAifQ.iIcIm23CCTcxNk_cim4JkSZnUCWC5yjbHfqCAXTuF9w', //staging POCPROD
     });
 
     const session = await cameraKit.createSession({ liveRenderTarget });
     const { lenses } = await cameraKit.lensRepository.loadLensGroups([
-        'f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b',
+        '8b4ee0f8-4839-4162-a17c-dc032788a7db',
     ]);
 
     session.applyLens(lenses[0]);
